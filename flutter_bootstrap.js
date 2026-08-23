0978f089081fe9feca3ea779a5440e638df36a3f
yusufzaidan72-crypto <yusufzaidan72-crypto@users.noreply.github.com>
2026-08-23 22:27:21 +0000

    deploy: 78742fb2b8b2b6ab83f10df543e9cc9fb03a476b

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"5d531788691ec3404cac0cee66ead4007b1773
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "37929327" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1345650077" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
