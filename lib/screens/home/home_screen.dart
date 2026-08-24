95a4cfda5daf12c7a7b6e2bf0d61d7020143ba0e
Yusuf Zaidan <yusufzaidan72@gmail.com>
2026-08-24 06:00:21 +0530

    fix: enforce const constructor for AgriImage and verify 0 issues

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,5 +1,4 @@
 import 'dart:ui';
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_animate/flutter_animate.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -81,6 +80,8 @@ class _HomeScreenState extends State<HomeScreen> {
             keywordOrUrl: 'farm',
             fit: BoxFit.cover,
           ),
+
+
 
 
           // Gradient overlay
